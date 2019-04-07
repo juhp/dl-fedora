de054fcccfb10f4f42ca9d8c4b26cd8aac902edd
Jens Petersen <petersen@redhat.com>
2019-04-07 23:40:55 +0800

    reworked for mirror redirects; --mirror; download with wget
    - use http-client instead of http-conduit, with response chunking
    - download with xdg Downloads dir with wget -c (noisy)
    - add --dryrun and --mirror (without path uses /pub/fedora/linux)
    - use simple-cmd and simple-cmd-args

--- Main.hs
+++ Main.hs
@@ -2,17 +2,24 @@
 
 import BasicPrelude
 
-import Network.HTTP.Simple (httpLBS, getResponseBody, getResponseHeaders, 
-                            getResponseStatusCode, parseRequest)
+import qualified Data.ByteString.Char8 as B
+
+import Network.HTTP.Client (brConsume, hrFinalResponse, hrRedirects, newManager, parseRequest, responseBody, responseHeaders, responseOpenHistory, responseStatus)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Types (decodePathSegments, extractPath, statusCode)
 
 import qualified Data.Text as T
 
 import Options.Applicative (auto)
-import Text.HTML.DOM (parseLBS)
+import Text.HTML.DOM (parseBSChunks)
 import Text.XML.Cursor
 
+import SimpleCmd ((.$))
 import SimpleCmdArgs
 --import Paths_fedora_iso_dl (version)
+
+import System.Directory (setCurrentDirectory)
+import System.Environment.XDG.UserDir (getUserDir)
 
 data FedoraEdition = Cloud
                    | Container
@@ -27,41 +34,59 @@ main :: IO ()
 main =
   simpleCmdArgs Nothing "Fedora iso downloader" 
     "Tool for downloading Fedora iso file images" $
-    findISO <$> 
-    strOptionalWith 'a' "arch" "ARCH" "architecture (default x86_64)" "x86_64" <*> 
-    optionalWith auto 'e' "edition" "EDITION" "Fedora edition (Workstation [default], Server, ...)" Workstation <*> 
-    argumentWith auto "RELEASE"
+    findISO
+    <$> switchWith 'n' "dry-run" "Don't actually download anything"
+    <*> strOptionalWith 'm' "mirror" "HOST" "default https://download.fedoraproject.org" "https://download.fedoraproject.org"
+    <*> strOptionalWith 'a' "arch" "ARCH" "architecture (default x86_64)" "x86_64"
+    <*> optionalWith auto 'e' "edition" "EDITION" "Fedora edition (Workstation [default], Server, ...)" Workstation
+    <*> argumentWith auto "RELEASE"
 
-findISO :: String -> FedoraEdition -> Int -> IO ()
-findISO arch edition release = do
-  let (relpath, released) = case release of
+findISO :: Bool -> String -> String -> FedoraEdition -> Int -> IO ()
+findISO dryrun host arch edition release = do
+  let (relpath, _released) = case release of
                30 -> ("development/30", False)
                _ -> if release > 30
                     then ("development/rawhide", False)
                     else ("releases" </> show release, True)
-      toppath = "pub/fedora/linux" 
+      toppath = if ((decodePathSegments . extractPath) (B.pack host) == [])
+                then "pub/fedora/linux"
+                else ""
       path = toppath </> relpath </> show edition </> arch </> editionMedia edition
-  unless released $ checkDir "https://dl.fedoraproject.org" path
-  checkDir "https://download.fedoraproject.org" path
+  fileurl <- checkURL path
+  if dryrun then putStrLn $ T.pack fileurl
+    else do
+    dlDir <- getUserDir "DOWNLOAD"
+    setCurrentDirectory dlDir
+    putStrLn ""
+    "wget" .$ ["-c", fileurl]
+    -- "curl" .$ ["-C", "-", "-L", "-O", fileurl]
   where
-    checkDir :: Text -> String -> IO ()
-    checkDir host path = do
-      let url = T.unpack host </> path
-      response <- parseRequest url >>= httpLBS
-      if getResponseStatusCode response /= 200
-        then do
-        print url
-        print $ getResponseHeaders response
+    checkURL :: String -> IO String
+    checkURL path = do
+      let url = host </> path
+      req <- parseRequest url
+      mgr <- newManager tlsManagerSettings
+      respHist <- responseOpenHistory req mgr
+      let redirect = listToMaybe . reverse $ mapMaybe (lookup "Location" . responseHeaders . snd) $ hrRedirects respHist
+      let finalUrl = maybe url B.unpack redirect
+      when (isJust redirect) $ putStr "Redirected to "
+      putStrLn $ T.pack finalUrl
+      let response = hrFinalResponse respHist
+      if statusCode (responseStatus response) /= 200
+        then
+        error $ show $ responseStatus response
         else do
-        let cursor = fromDocument $ parseLBS $ getResponseBody response
-            hrefs = concatMap (attribute "href") $ cursor $// element "pre" &// element "a" -- &/ attribute "href"
-            res = filter (editionPrefix edition `T.isPrefixOf`) hrefs :: [Text]
-        case res of
-          [] -> do
-            putStrLn $ "not found " <> T.pack url
-            mapM_ putStrLn hrefs
-            print $ getResponseHeaders response
-          files -> mapM_ (putStrLn . (<> " (" <> host <> ")")) files
+        body <- brConsume $ responseBody response
+        let doc = parseBSChunks body
+            cursor = fromDocument doc
+            hrefs = concatMap (attribute "href") $ cursor $// element "a"
+            mfile = listToMaybe $ filter (editionPrefix edition `T.isPrefixOf`) hrefs :: Maybe Text
+        case mfile of
+          Nothing -> do
+            print doc
+            error $ "not found " ++ finalUrl
+          Just file ->
+            return $ finalUrl </> T.unpack file
 
 editionPrefix :: FedoraEdition -> Text
 editionPrefix Workstation = "Fedora-Workstation-Live"
