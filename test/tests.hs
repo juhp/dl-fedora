32b7f9598133e93c0cd77e7bb19142d70a4377c0
Jens Petersen <petersen@redhat.com>
2021-04-07 00:57:45 +0800

    convert test.sh into test/test.hs
    It was running old cabal v1 path

---
+++ test/tests.hs
@@ -0,0 +1,22 @@
+import SimpleCmd
+
+dlFedora :: [String] -> IO ()
+dlFedora args =
+  putStrLn "" >> cmdLog "dl-fedora" args
+
+tests :: [[String]]
+tests =
+  [["-n", "33", "-c"]
+  ,["-n", "rawhide", "-e", "silverblue"]
+  ,["-n", "34", "-e", "silverblue"]
+  ,["respin"]
+  ,["-n", "32", "-e", "kde"]
+  ,["-n", "33", "-e", "everything"]
+  ,["-n", "33", "-e", "server", "--arch", "aarch64"]
+  ,["-n", "34"]
+  ]
+
+main :: IO ()
+main = do
+  mapM_ dlFedora tests
+  putStrLn $ show (length tests) ++ " tests run"
