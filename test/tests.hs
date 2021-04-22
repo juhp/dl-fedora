4811c777b031cb249dc0483202cd76e7a33d2cee
Jens Petersen <petersen@redhat.com>
2021-04-22 12:39:58 +0800

    tests: reduce the number of testcases when GITHUB_ACTIONS
    seeing frequent timeouts: dunno if longer timeouts would help

--- test/tests.hs
+++ test/tests.hs
@@ -1,23 +1,30 @@
+import Data.Maybe
+import System.Environment (lookupEnv)
 import SimpleCmd
 
 dlFedora :: [String] -> IO ()
 dlFedora args =
   putStrLn "" >> cmdLog "dl-fedora" args
 
-tests :: [[String]]
-tests =
+tests :: Maybe String -> [[String]]
+tests mGhAction =
   [["-n", "33", "-c"]
   ,["-n", "rawhide", "silverblue"]
-  ,["-n", "34", "silverblue"]
   ,["-n", "respin"]
-  ,["-n", "32", "kde"]
-  ,["-n", "33", "everything"]
-  ,["-n", "33", "server", "--arch", "aarch64"]
   ,["-l", "34"]
   ,["-l", "rawhide", "-n"]
-  ]
+  ] ++
+  if isJust mGhAction then []
+  else
+    [["-n", "34", "silverblue"]
+    ,["-n", "32", "kde"]
+    ,["-n", "33", "everything"]
+    ,["-n", "33", "server", "--arch", "aarch64"]
+    ]
 
 main :: IO ()
 main = do
-  mapM_ dlFedora tests
-  putStrLn $ show (length tests) ++ " tests run"
+  mGhAction <- lookupEnv "GITHUB_ACTIONS"
+  let cases = tests mGhAction
+  mapM_ dlFedora cases
+  putStrLn $ show cases ++ " tests run"
